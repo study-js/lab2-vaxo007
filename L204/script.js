0217f57bca84e7cfe0d66b966a4b598ec7c02d7a
gigga1997 <gigga1997@gmail.com>
2017-11-08 18:40:43 +0400

    l204

---
+++ L204/script.js
@@ -0,0 +1,15 @@
+var password = "vaxtangi";
+var sheyvana = prompt("შეიყვანეთ პაროლი: ")
+
+
+while (sheyvana !== "vaxtangi"){
+    
+
+    sheyvana = prompt("შეიყვანეთ პაროლი: ");
+
+    
+}
+
+document.write("თქვენ შეხვედით!")
+
+
